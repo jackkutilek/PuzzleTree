67ad69bcb596ab546af7b397d903429b909a5a77
Jack Kutilek <jack.kutilek@gmail.com>
2023-02-25 10:25:08 -0800

    screenshake

--- addons/PuzzleTree/utils/PTCamera.gd
+++ addons/PuzzleTree/utils/PTCamera.gd
@@ -11,6 +11,7 @@ export(float) var snap_size = 0.0
 
 var level_size = Vector2(1,1)
 var target = Vector2(0,0)
+var follow_offset = Vector2(0,0)
 
 # --------------------------------------------------------------------------------------------------
 
@@ -20,6 +21,7 @@ func late_init_update(context):
 			update_level_size(context)
 		"scripted":
 			update_target(context)
+			follow_offset = target
 			offset = target
 
 func late_frame_update(context):
@@ -48,11 +50,22 @@ func _process(delta):
 			var zoomy = target_size / size.y
 			var z = max(zoomx, zoomy)
 			zoom = Vector2(z,z)
-			var diff = target - offset
+			var diff = target - follow_offset
 			if eased_follow:
-				offset = offset + diff*camera_speed*delta
+				follow_offset = follow_offset + diff*camera_speed*delta
 			else:
-				offset = target
+				follow_offset = target
+	
+	if trauma:
+		trauma = max(trauma - decay * delta, 0)
+		_shake()
+	offset = follow_offset + shake_offset
+
+func _ready():
+	randomize()
+	noise.seed = randi()
+	noise.period = 4
+	noise.octaves = 2
 
 # --------------------------------------------------------------------------------------------------
 
@@ -82,4 +95,30 @@ func fit_to_level():
 	var zoomy = level_size.y / size.y
 	var z = max(zoomx, zoomy)
 	zoom = Vector2(z,z)
-	offset = Vector2(level_size.x/2, level_size.y/2)
+	follow_offset = Vector2(level_size.x/2, level_size.y/2)
+
+# ----------------------------------------------------------------------------------------------
+
+export var decay = 0.8  # How quickly the shaking stops [0, 1].
+export var max_offset = Vector2(100, 75)  # Maximum hor/ver shake in pixels.
+export var max_roll = 0.1  # Maximum rotation in radians (use sparingly).
+
+var trauma = 0.0  # Current shake strength.
+var trauma_power = 2  # Trauma exponent. Use [2, 3].
+
+onready var noise = OpenSimplexNoise.new()
+var noise_y = 0
+var shake_offset = Vector2(0,0)
+
+func shake(amount):
+	print("shake ", amount)
+	trauma = amount
+	noise_y = 0
+
+func _shake():
+	var amount = pow(trauma, trauma_power)
+	noise_y += 1
+	rotation = max_roll * amount * noise.get_noise_2d(noise.seed, noise_y)
+	shake_offset.x = max_offset.x * amount * noise.get_noise_2d(noise.seed*2, noise_y)
+	shake_offset.y = max_offset.y * amount * noise.get_noise_2d(noise.seed*3, noise_y)
+	pass

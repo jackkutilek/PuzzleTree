8dc2209cba53d97b4abceba3634efa17c7ff6df7
Jack Kutilek <jack.kutilek@gmail.com>
2023-02-28 17:45:58 -0800

    add zoom controls to PTCamera. and allow rectangular snap grid

--- addons/PuzzleTree/utils/PTCamera.gd
+++ addons/PuzzleTree/utils/PTCamera.gd
@@ -4,10 +4,10 @@ class_name PTCamera, "../icons/PTCamera.png"
 export var tile_size = 5
 export(String, "auto", "scripted") var mode = "auto"
 
-export var target_size = 100
+export var target_size = Vector2(10,10)
 export var eased_follow = false
 export var camera_speed = 1
-export(float) var snap_size = 0.0
+export(Vector2) var snap_size = Vector2(0,0)
 
 var level_size = Vector2(1,1)
 var target = Vector2(0,0)
@@ -23,6 +23,8 @@ func late_init_update(context):
 			update_target(context)
 			follow_offset = target
 			offset = target
+			
+			zoom = get_target_zoom()
 
 func late_frame_update(context):
 	match mode:
@@ -45,11 +47,10 @@ func _process(delta):
 		"auto":
 			fit_to_level()
 		"scripted":
-			var size = get_viewport().size
-			var zoomx = target_size / size.x
-			var zoomy = target_size / size.y
-			var z = max(zoomx, zoomy)
-			zoom = Vector2(z,z)
+			var z = get_target_zoom()
+			var zdiff = z - zoom
+			zoom += zdiff*camera_speed*delta
+			
 			var diff = target - follow_offset
 			if eased_follow:
 				follow_offset = follow_offset + diff*camera_speed*delta
@@ -72,13 +73,15 @@ func _ready():
 func update_target(context):
 	if context.has("camera_pos"):
 		var camera_pos = context.camera_pos
-		if camera_pos == null:
-			return
-		if snap_size > 0:
-			camera_pos = Vector2(floor(camera_pos.x/snap_size)*snap_size + snap_size/2, floor(camera_pos.y/snap_size)*snap_size + snap_size/2)
-		target = camera_pos * tile_size
-		if snap_size > 0:
-			target_size = snap_size*tile_size
+		if camera_pos != null:
+			if snap_size.x > 0:
+				camera_pos = Vector2(floor(camera_pos.x/snap_size.x)*snap_size.x + snap_size.x/2, floor(camera_pos.y/snap_size.y)*snap_size.y + snap_size.y/2)
+			target = camera_pos * tile_size
+	
+	if context.has("camera_size"):
+		var camera_size = context.camera_size
+		if camera_size != null:
+			target_size = camera_size
 
 func update_level_size(context):
 	if not context.has("_level_width"):
@@ -89,7 +92,14 @@ func update_level_size(context):
 	var height = context._level_height * tile_size
 	level_size = Vector2(width, height)
 
-func fit_to_level():	
+func get_target_zoom():
+	var size = get_viewport().size
+	var zoomx = target_size.x * tile_size / size.x
+	var zoomy = target_size.y * tile_size / size.y
+	var z = max(zoomx, zoomy)
+	return Vector2(z,z)
+
+func fit_to_level():
 	var size = get_viewport().size
 	var zoomx = level_size.x / size.x
 	var zoomy = level_size.y / size.y
